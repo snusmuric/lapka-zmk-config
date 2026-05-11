407d461538cfde653823208ec2158238a75098ff
Sergey Boykov <sergsv03@gmail.com>
2026-05-12 00:00:58 +0200

    migration to the newest version of zmk

--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ cd "$ZMK_DIR"
 
 rm -rf "$SCRIPT_DIR/build" "$SCRIPT_DIR/out"
 echo "Building left half..."
-west build -s app -d "$SCRIPT_DIR/build/left"  -b nice_nano_v2 -- -DSHIELD=lapka_left  -DZMK_CONFIG="$SCRIPT_DIR/config" -DZMK_EXTRA_MODULES="$SCRIPT_DIR"
+west build -s app -d "$SCRIPT_DIR/build/left"  -b nice_nano -- -DSHIELD=lapka_left  -DZMK_CONFIG="$SCRIPT_DIR/config" -DZMK_EXTRA_MODULES="$SCRIPT_DIR"
 echo "  -> $SCRIPT_DIR/build/left/zephyr/zmk.uf2"
 
 echo ""
 echo "Building right half..."
-west build -s app -d "$SCRIPT_DIR/build/right" -b nice_nano_v2 -- -DSHIELD=lapka_right -DZMK_CONFIG="$SCRIPT_DIR/config" -DZMK_EXTRA_MODULES="$SCRIPT_DIR"
+west build -s app -d "$SCRIPT_DIR/build/right" -b nice_nano -- -DSHIELD=lapka_right -DZMK_CONFIG="$SCRIPT_DIR/config" -DZMK_EXTRA_MODULES="$SCRIPT_DIR"
 echo "  -> $SCRIPT_DIR/build/right/zephyr/zmk.uf2"
 
 echo ""
 echo "Building settings reset..."
-west build -s app -d "$SCRIPT_DIR/build/reset" -b nice_nano_v2 -- -DSHIELD=settings_reset -DZMK_CONFIG="$SCRIPT_DIR/config" -DZMK_EXTRA_MODULES="$SCRIPT_DIR"
+west build -s app -d "$SCRIPT_DIR/build/reset" -b nice_nano -- -DSHIELD=settings_reset -DZMK_CONFIG="$SCRIPT_DIR/config" -DZMK_EXTRA_MODULES="$SCRIPT_DIR"
 echo "  -> $SCRIPT_DIR/build/reset/zephyr/zmk.uf2"
 
 echo ""
